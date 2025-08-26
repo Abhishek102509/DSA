3bf5f23f2efb7d8f488a6c5b2ff3d6cd5caef316
Abhishek yadav <128358062+Abhishek102509@users.noreply.github.com>
2025-08-26 23:59:11 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (77.84%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -23,31 +23,50 @@
 //     }
 // }
 
+
+
+//BFS
+// class Solution {
+//     public int maxDepth(TreeNode root) {
+//       if(root == null){
+//         return 0;
+//       }
+
+//       Queue<TreeNode> queue = new LinkedList<>();
+//       queue.add(root);
+//       int depth = 0;
+
+//       while(!queue.isEmpty()){
+//         depth++;
+//         int levelSize = queue.size();
+
+//         for(int i =0; i < levelSize; i++){
+//             TreeNode node = queue.poll();
+//             if (node.left != null) {
+//                     queue.add(node.left);
+//                 }
+//                 if (node.right != null) {
+//                     queue.add(node.right);
+//                 }
+//         }
+//       }
+//       return depth;
+
+//     }
+// }
+
+
+
 class Solution {
     public int maxDepth(TreeNode root) {
-      if(root == null){
-        return 0;
-      }
+        if(root == null) return 0;
 
-      Queue<TreeNode> queue = new LinkedList<>();
-      queue.add(root);
-      int depth = 0;
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
 
-      while(!queue.isEmpty()){
-        depth++;
-        int levelSize = queue.size();
+        int totalDepth = Math.max(leftDepth , rightDepth)+1;
 
-        for(int i =0; i < levelSize; i++){
-            TreeNode node = queue.poll();
-            if (node.left != null) {
-                    queue.add(node.left);
-                }
-                if (node.right != null) {
-                    queue.add(node.right);
-                }
-        }
-      }
-      return depth;
+        return totalDepth;
 
     }
 }
