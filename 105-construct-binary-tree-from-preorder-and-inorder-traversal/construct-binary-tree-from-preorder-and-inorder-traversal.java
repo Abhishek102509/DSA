2a6c1bcd46f8de6a07275dbeb68e943062c4e60a
Abhishek yadav <128358062+Abhishek102509@users.noreply.github.com>
2025-08-29 13:26:05 +0530

    Time: 1 ms (96.37%) | Memory: 44.6 MB (40.84%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -13,31 +13,63 @@
  *     }
  * }
  */
-class Solution {
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
-        Deque<Integer> preorderQueue = new ArrayDeque<>();
-        for(int val: preorder){
-            preorderQueue.offer(val);
-        }
-        return build(preorderQueue,inorder);
-    }
-    private TreeNode build(Deque<Integer>preorder , int[] inorder){
-         if(inorder.length>0){
-            int idx = indexOf(inorder,preorder.poll());
-            TreeNode root = new TreeNode(inorder[idx]);
+// class Solution {
+//     public TreeNode buildTree(int[] preorder, int[] inorder) {
+//         Deque<Integer> preorderQueue = new ArrayDeque<>();
+//         for(int val: preorder){
+//             preorderQueue.offer(val);
+//         }
+//         return build(preorderQueue,inorder);
+//     }
+//     private TreeNode build(Deque<Integer>preorder , int[] inorder){
+//          if(inorder.length>0){
+//             int idx = indexOf(inorder,preorder.poll());
+//             TreeNode root = new TreeNode(inorder[idx]);
 
-            root.left =build(preorder, Arrays.copyOfRange(inorder, 0, idx));
-            root.right = build(preorder, Arrays.copyOfRange(inorder, idx +              1,  inorder.length));
-            return root;
-         }
-         return null;
-    }
-    private int indexOf(int[] arr, int value){
-        for(int i =0;i<arr.length; i++){
-            if(arr[i] == value){
-                return i;
-            }
+//             root.left =build(preorder, Arrays.copyOfRange(inorder, 0, idx));
+//             root.right = build(preorder, Arrays.copyOfRange(inorder, idx +              1,  inorder.length));
+//             return root;
+//          }
+//          return null;
+//     }
+//     private int indexOf(int[] arr, int value){
+//         for(int i =0;i<arr.length; i++){
+//             if(arr[i] == value){
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
+
+
+
+
+class Solution {
+    private int preorderIndex;
+    private Map<Integer, Integer> mapping;
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        mapping = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) {
+            mapping.put(inorder[i], i);
         }
-        return -1;
+
+        preorderIndex = 0;
+        return build(preorder, 0, inorder.length - 1);        
+    }
+
+    private TreeNode build(int[] preorder, int start, int end) {
+        if (start > end) return null;
+
+        int rootVal = preorder[preorderIndex++];
+        TreeNode root = new TreeNode(rootVal);
+        int mid = mapping.get(rootVal);
+
+        root.left = build(preorder, start, mid - 1);
+        root.right = build(preorder, mid + 1, end);
+
+        return root;
     }
 }
